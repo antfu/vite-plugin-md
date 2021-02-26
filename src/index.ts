4c96ded8ca28928070de76227f87e19a1140a2f0
Rémi Alvergnat <toilal.dev@gmail.com>
2021-02-26 14:00:19 +0100

    feat: add vue2 support (#9)

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import type { Plugin, ResolvedConfig } from 'vite'
+import type { Plugin } from 'vite'
 import { Options } from './types'
 import { createMarkdown } from './markdown'
 import { resolveOptions } from './options'
@@ -7,29 +7,19 @@ function VitePluginMarkdown(userOptions: Options = {}): Plugin {
   const options = resolveOptions(userOptions)
   const markdownToVue = createMarkdown(options)
 
-  let vuePlugin: Plugin
-
   return {
     name: 'vite-plugin-md',
     enforce: 'pre',
-    configResolved(config) {
-      vuePlugin = config.plugins.find(p => p.name === 'vite:vue')!
-      if (!vuePlugin)
-        throw new Error('[vite-plugin-md] no vue plugin found, do you forget to install it?')
-    },
     transform(raw, id) {
       if (id.endsWith('.md'))
         return markdownToVue(id, raw)
     },
     async handleHotUpdate(ctx) {
-      // hot reload .md files as .vue files
       if (ctx.file.endsWith('.md')) {
-        return vuePlugin.handleHotUpdate!({
-          ...ctx,
-          async read() {
-            return markdownToVue(ctx.file, await ctx.read())
-          },
-        })
+        const defaultRead = ctx.read
+        ctx.read = async function() {
+          return markdownToVue(ctx.file, await defaultRead())
+        }
       }
     },
   }
