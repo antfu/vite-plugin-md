9362cecf6a698cf47ba6065edcc46c4d99e3e58f
Ken Snyder <ken@ken.net>
2022-03-07 15:00:52 -0800

    chore: add simple test to show usage

---
+++ test/frontmatterPreprocessor.test.ts
@@ -0,0 +1,52 @@
+import { readFile } from 'fs/promises'
+import { describe, expect, it } from 'vitest'
+import { createMarkdown } from '../src/markdown'
+import { resolveOptions } from '../src/options'
+import { Frontmatter, ResolvedOptions, MetaProperty } from '../src/types'
+
+const frontmatterPreprocess: ResolvedOptions['frontmatterPreprocess'] = (fm) => {
+  const frontmatter = {
+    title: 'default title',
+    description: 'default description',
+    ...fm,
+  }
+  const meta: MetaProperty[] = [
+    { property: 'og:title', name: 'twitter:title', itemprop: 'title', content: frontmatter.title },
+    {
+      property: 'og:description',
+      name: 'twitter:description',
+      itemprop: 'description',
+      content: frontmatter.description,
+    },
+  ]
+  return {
+    head: { ...frontmatter, meta },
+    frontmatter: { ...frontmatter, meta },
+  }
+}
+
+describe('provide bespoke frontmatter processor', () => {
+  it('inline markdown is used over default properties', async () => {
+    const parser = createMarkdown(resolveOptions({ frontmatterPreprocess }))
+    const md = parser('', await readFile('test/fixtures/simple.md', 'utf-8'))
+    // Positive tests
+    expect(
+      md.includes('Hello World'),
+      "the title attribute is retained over the default 'title' value"
+    ).toBeTruthy()
+    expect(
+      md.includes('testing is the path to true happiness'),
+      'description property is also retained'
+    ).toBeTruthy()
+    // Negative tests
+    expect(
+      md.includes('default title'),
+      "the title attribute is retained over the default 'title' value"
+    ).toBeFalsy()
+    expect(md.includes('default description'), 'default description is ignored').toBeFalsy()
+
+    // Meta props
+    expect(md.includes('og:title')).toBeTruthy()
+    expect(md.includes('og:description')).toBeTruthy()
+  })
+})
