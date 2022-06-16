a501dfe6b8eb3b8195c0658a4ee0fe4465e5609c
Percy Ma <kecrily@gmail.com>
2022-06-16 22:17:04 +0800

    chore: update test

--- test/wrapper-classes.test.ts
+++ test/wrapper-classes.test.ts
@@ -14,37 +14,31 @@ describe('Wrapper classes work as expected', () => {
     expect(sfc.html).toContain('markdown-body')
   })
 
+  it('changing the value of the \'wrapperClasses\' config to add styles to the original style', async () => {
+    const sfc = await composeSfcBlocks('wrapper.md', md, { wrapperClasses: 'markdown-body test-wrap' })
+    expect(sfc.html).toContain('test-wrap')
+    expect(sfc.html).toContain('markdown-body')
+    expect(sfc.html).toContain(':root')
+  })
+
   it('changing the value of the \'wrapperClasses\' configuration is reflected in the resulting HTML output', async () => {
     const sfc = await composeSfcBlocks('wrapper.md', md, { wrapperClasses: 'test-wrap' })
     expect(sfc.html).toContain('test-wrap')
     expect(sfc.html).not.toContain('markdown-body')
+    expect(sfc.html).not.toContain(':root')
   })
 
   it('changing the value of the \'wrapperClasses\' config to include multiple classes', async () => {
     const sfc = await composeSfcBlocks('wrapper.md', md, { wrapperClasses: 'prose m-auto' })
     expect(sfc.html).toContain('prose m-auto')
     expect(sfc.html).not.toContain('markdown-body')
+    expect(sfc.html).not.toContain(':root')
   })
 
   it('changing the value of the \'wrapperClasses\' config along with \'wrapperComponent\'', async () => {
     const sfc = await composeSfcBlocks('wrapper.md', md, { wrapperClasses: 'prose m-auto', wrapperComponent: 'post' })
     expect(sfc.html).toContain('prose m-auto')
     expect(sfc.html).not.toContain('markdown-body')
-  })
-})
-
-describe('Snapshots for wrapper-testing', () => {
-  beforeAll(async () => {
-    md = await readFile('test/fixtures/simple.md', 'utf-8')
-  })
-
-  it('html is consistent for default config', async () => {
-    const { html } = await composeSfcBlocks('/foobar/meta.md', md)
-    expect(html).toMatchSnapshot()
-  })
-
-  it('html is consistent for explicit config', async () => {
-    const { html } = await composeSfcBlocks('wrapper.md', md, { wrapperClasses: 'prose m-auto', wrapperComponent: 'post' })
-    expect(html).toMatchSnapshot()
+    expect(sfc.html).not.toContain(':root')
   })
 })
